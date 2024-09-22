e5ed714f6f4752232e4d532c6c85f0c26921a955
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2024-09-22 16:09:40 +0100

    fix

--- template/components/Button/Button.stories.tsx
+++ template/components/Button/Button.stories.tsx
@@ -1,10 +1,10 @@
 // stories/MyButton.stories.tsx
-import type { Meta, StoryObj } from "@storybook/react";
-
-import { MyButton } from "./Button";
+import type {Meta, StoryObj} from '@storybook/react';
+import {action} from '@storybook/addon-actions';
+import {MyButton} from './Button';
 
 const meta = {
-  title: "components/MyButton",
+  title: 'components/MyButton',
   component: MyButton,
 } satisfies Meta<typeof MyButton>;
 
@@ -14,7 +14,8 @@ type Story = StoryObj<typeof meta>;
 
 export const Basic: Story = {
   args: {
-    text: "Hello World",
-    color: "purple",
+    text: 'Hello World',
+    color: 'purple',
+    onPress: action('onPress'),
   },
 };
