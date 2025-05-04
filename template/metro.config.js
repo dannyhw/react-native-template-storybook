2087909a27a3d8a6d32f59fd9c002618258b4d5c
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2025-05-04 20:28:38 +0100

    feat: update react native (#12)
    * feat: v9 update
    
    * 2.0.0-0
    
    * fix
    
    * 2.0.1-0
    
    * fix
    
    * 2.0.1
    
    * fix
    
    * 2.0.2
    
    * fix
    
    * 2.0.3
    
    * fix
    
    * 2.0.4
    
    * fix

--- template/metro.config.js
+++ template/metro.config.js
@@ -8,34 +8,14 @@ const defaultConfig = getDefaultConfig(__dirname);
  * Metro configuration
  * https://facebook.github.io/metro/docs/configuration
  *
- * @type {import('metro-config').MetroConfig}
+ * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {
-  resolver: {
-    resolveRequest: (context, moduleName, platform) => {
-      const defaultResolveResult = context.resolveRequest(
-        context,
-        moduleName,
-        platform
-      );
-
-      if (
-        process.env.STORYBOOK_ENABLED !== "true" &&
-        defaultResolveResult?.filePath?.includes?.(".ondevice/")
-      ) {
-        return {
-          type: "empty",
-        };
-      }
-
-      return defaultResolveResult;
-    },
-  },
-};
+const config = {};
 
 const finalConfig = mergeConfig(defaultConfig, config);
 
 module.exports = withStorybook(finalConfig, {
     enabled: process.env.STORYBOOK_ENABLED === 'true',
-    configPath: path.resolve(__dirname, './.ondevice'),
+    onDisabledRemoveStorybook: true,
+    configPath: path.resolve(__dirname, './.rnstorybook'),
 });
